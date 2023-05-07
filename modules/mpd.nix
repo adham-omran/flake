5fb78c83fc0e9a068a71e85c40df959f96a1b7fa
adham <git@adham-omran.com>
2023-05-07 22:23:04 +0300

    Add mpd.nix module
    Note that it is still commented out in the imports due to issues with rebuilds
    and the mpd socket that I do not have time to fix at the moment.

---
+++ modules/mpd.nix
@@ -0,0 +1,26 @@
+{...}:
+{
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/home/adham/music";
+    extraConfig = ''
+audio_output {
+    type "pipewire"
+    name "My PipeWire Output"
+}
+  '';
+    # Optional:
+    network.listenAddress = "any"; # if you want to allow non-localhost
+                                   # connections
+    startWhenNeeded = true; # systemd feature: only start MPD service upon
+                            # connection to its socket
+  };
+  services.mpd.user = "userRunningPipeWire";
+  systemd.services.mpd.environment = {
+    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+    XDG_RUNTIME_DIR = "/run/user/1000"; # User-id 1000 must match above user.
+                                        # MPD will look inside this directory
+                                        # for the PipeWire socket.
+  };
+
+}
