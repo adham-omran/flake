0df7ae68781da8c10794d8a3487c0752b4e2475b
adham <git@adham-omran.com>
2023-05-07 11:58:06 +0300

    Extract GNOME configuration

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
       ./modules/docker.nix
       ./modules/flatpak.nix
       ./modules/kanata.nix
+      ./modules/gnome.nix
+      # ./modules/kde.nix
       # ./mpd.nix
     ];
 
@@ -58,31 +60,7 @@
     };
   };
 
-  ## GNOME
-  # services.xserver.displayManager.startx.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-  programs.dconf.enable = true;
 
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-  ]) ++ (with pkgs.gnome; [
-    nautilus # Files, replaced with Nemo
-    cheese # webcam tool
-    gnome-music
-    gnome-terminal
-    gedit # text editor
-    epiphany # web browser
-    geary # email reader
-    gnome-characters
-    totem # video player
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
-  ]);
   ## END OF GNOME
 
   # Configure keymap in X11
---
+++ modules/gnome.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, callPackage, lib, ... }:
+{
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  programs.dconf.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    nautilus # Files, replaced with Nemo
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
+}
