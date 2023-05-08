c6a1514a398908fc2b9d0f814f17538b66700db3
adham <git@adham-omran.com>
2023-05-08 18:43:37 +0300

    Update charging thresholds

--- modules/power.nix
+++ modules/power.nix
@@ -5,9 +5,9 @@
   services.tlp.enable = true;
   services.tlp.settings = {
     START_CHARGE_THRESH_BAT0=75;
-    STOP_CHARGE_THRESH_BAT0=80;
+    STOP_CHARGE_THRESH_BAT0=95;
 
     START_CHARGE_THRESH_BAT1=75;
-    STOP_CHARGE_THRESH_BAT1=80;
+    STOP_CHARGE_THRESH_BAT1=95;
   };
 }
