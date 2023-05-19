77b7f638cc1dfc922c77ba78d30da1ee2de001c7
adham <git@adham-omran.com>
2023-05-19 13:30:24 +0300

    Add sway.nix

---
+++ modules/sway.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+let
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+  systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+  systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      '';
+  };
+in
+{
+  environment.systemPackages = with pkgs; [
+    dbus-sway-environment
+  ];
+}
