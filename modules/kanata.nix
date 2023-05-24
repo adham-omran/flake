ba00cfa8b472be606abe06a64dcb469058003aaf
adham <git@adham-omran.com>
2023-05-24 10:43:38 +0300

    Update kanata configuration

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -29,10 +29,16 @@
         i (tap-hold $tt $ht i ralt)
         o (tap-hold $tt $ht o rmet)
 
+        0 (tap-hold $tt $ht 0 M-0)
         1 (tap-hold $tt $ht 1 M-1)
         2 (tap-hold $tt $ht 2 M-2)
         3 (tap-hold $tt $ht 3 M-3)
         4 (tap-hold $tt $ht 4 M-4)
+        5 (tap-hold $tt $ht 5 M-5)
+        6 (tap-hold $tt $ht 6 M-6)
+        7 (tap-hold $tt $ht 7 M-7)
+        8 (tap-hold $tt $ht 8 M-8)
+        9 (tap-hold $tt $ht 9 M-9)
      )
 
       (defsrc
@@ -46,7 +52,7 @@
 
       (deflayer colemak
         esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  del
-        grv  @1   @2   @3   @4    5    6    7    8    9    0    -    =    bspc
+        grv  @1   @2   @3   @4   @5   @6   @7   @8   @9   @0    -    =    bspc
         tab  q    w    f    p    g    j    l    u    y    ;    [    ]    \
         caps @a   @r   @s  @t    d    h   @n   @e   @i    @o    '    ret
         lsft z    x    c    v    b    k    m    ,    .    /    rsft
