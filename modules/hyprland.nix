f491fbe246ac9dfd4cd60b8146bacd7be09008d1
adham-omran <git@adham-omran.com>
2023-05-29 09:00:52 +0300

    Extract the hyprland-related packages

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -3,4 +3,13 @@
   programs.hyprland = {
     enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    hyprland
+    hyprland-protocols
+    hyprland-share-picker
+    hyprpaper
+    xdg-desktop-portal-hyprland
+    waybar
+  ];
 }
