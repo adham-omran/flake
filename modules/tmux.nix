ef6d3a8c3af7f8b25f4417a8bd6e315b6933b3fd
adham-omran <git@adham-omran.com>
2023-05-29 09:01:28 +0300

    Enable tmux color configuration

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -15,12 +15,12 @@
       tmuxPlugins.better-mouse-mode
     ];
 
-    # extraConfig = ''
-    #   # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-    #   set -g default-terminal "xterm-256color"
-    #   set -ga terminal-overrides ",*256col*:Tc"
-    #   set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-    #   set-environment -g COLORTERM "truecolor"
+    extraConfig = ''
+    # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+    set -g default-terminal "xterm-256color"
+    set -ga terminal-overrides ",*256col*:Tc"
+    set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+    set-environment -g COLORTERM "truecolor"
 
     #   # Mouse works as expected
     #   set-option -g mouse on
@@ -28,7 +28,7 @@
     #   bind | split-window -h -c "#{pane_current_path}"
     #   bind - split-window -v -c "#{pane_current_path}"
     #   bind c new-window -c "#{pane_current_path}"
-    # '';
+    '';
   };
 
   # programs.tmate = {
