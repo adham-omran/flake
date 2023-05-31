0374d05874df729ff46931b7ccf003b6083cee1f
adham-omran <git@adham-omran.com>
2023-05-31 10:42:06 +0300

    Use noweb for mpd literate configuration

--- modules/mpd.nix
+++ modules/mpd.nix
@@ -4,20 +4,18 @@
     enable = true;
     musicDirectory = "/home/adham/music";
     extraConfig = ''
+    audio_output {
+      type "pipewire"
+      name "My PipeWire Output"
+    }
+    '';
 
-audio_output {
-  type "pipewire"
-  name "My PipeWire Output"
-}
+    network.listenAddress = "any";
+    startWhenNeeded = true;
+    };
 
-'';
-
-network.listenAddress = "any";
-startWhenNeeded = true;
-};
-
-services.mpd.user = "userRunningPipeWire";
-systemd.services.mpd.environment = {
-  XDG_RUNTIME_DIR = "/run/user/1000";
-};
+    services.mpd.user = "userRunningPipeWire";
+    systemd.services.mpd.environment = {
+      XDG_RUNTIME_DIR = "/run/user/1000";
+  };
 }
