51b90c22e9d512dc758efbb7ec03a2cd2ac4abc2
adham-omran <git@adham-omran.com>
2023-06-01 22:10:59 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,4 +1,8 @@
 {config, pkgs, lib, ...}:
+let
+  pdf-app="org.pwmt.zathura.desktop";
+  img-app="feh.desktop";
+in
 {
   home = {
     stateVersion = "23.05";
@@ -162,4 +166,37 @@
   };
   
   home.sessionVariables.GTK_THEME = "palenight";
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = ["${pdf-app}"];
+    };
+    defaultApplications = {
+      "application/pdf" = ["${pdf-app}"];
+      "image/bmp"= ["${img-app}"];
+      "image/gif"=["${img-app}"];
+      "image/jpg"=["${img-app}"];
+      "image/pjpeg"=["${img-app}"];
+      "image/png"=["${img-app}"];
+      "image/tiff"=["${img-app}"];
+      "image/webp"=["${img-app}"];
+      "image/x-bmp"=["${img-app}"];
+      "image/x-gray"=["${img-app}"];
+      "image/x-icb"=["${img-app}"];
+      "image/x-ico"=["${img-app}"];
+      "image/x-png"=["${img-app}"];
+      "image/x-portable-anymap"=["${img-app}"];
+      "image/x-portable-bitmap"=["${img-app}"];
+      "image/x-portable-graymap"=["${img-app}"];
+      "image/x-portable-pixmap"=["${img-app}"];
+      "image/x-xbitmap"=["${img-app}"];
+      "image/x-xpixmap"=["${img-app}"];
+      "image/x-pcx"=["${img-app}"];
+      "image/svg+xml"=["${img-app}"];
+      "image/svg+xml-compressed"=["${img-app}"];
+      "image/vnd.wap.wbmp"=["${img-app}"];
+      "image/x-icns"=["${img-app}"];
+      "x-scheme-handler/element"=["element-desktop.desktop"];
+    };
+  };
 }
