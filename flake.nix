54418949a92cdea43357ca8c30c50789f11ca1c3
adham-omran <git@adham-omran.com>
2023-12-01 19:55:21 +0300

    Indent code

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     {
       nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
       home-manager = {
-              url = "github:nix-community/home-manager/release-23.11";
-              inputs.nixpkgs.follows = "nixpkgs";
+        url = "github:nix-community/home-manager/release-23.11";
+        inputs.nixpkgs.follows = "nixpkgs";
       };
     };
 
@@ -14,53 +14,39 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
+        inherit system;
+        config.allowUnfree = true;
       };
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
-              nixos = lib.nixosSystem {
-                      inherit system;
-                      modules = [
-                              ./configuration.nix
-                              home-manager.nixosModules.home-manager {
-                                      home-manager.useGlobalPkgs = true;
-                                      home-manager.useUserPackages = true;
-                                      home-manager.users.adham = {
-                                              imports = [ ./home.nix ];
-                                      };
-                              }
-                      ];
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.adham = {
+                imports = [ ./home.nix ];
               };
+            }
+          ];
+        };
 
-              extraStuff = lib.nixosSystem {
-                      inherit system;
-                      modules = [
-                              ./basic/configuration.nix
-                              home-manager.nixosModules.home-manager {
-                                      home-manager.useGlobalPkgs = true;
-                                      home-manager.useUserPackages = true;
-                                      home-manager.users.adham = {
-                                              imports = [ ./home.nix ];
-                                      };
-                              }
-                      ];
+        t480 = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./t480/configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.adham = {
+                imports = [ ./home.nix ];
               };
-
-              t480 = lib.nixosSystem {
-                      inherit system;
-                      modules = [
-                              ./t480/configuration.nix
-                              home-manager.nixosModules.home-manager {
-                                      home-manager.useGlobalPkgs = true;
-                                      home-manager.useUserPackages = true;
-                                      home-manager.users.adham = {
-                                              imports = [ ./home.nix ];
-                                      };
-                              }
-                      ];
-              };
+            }
+          ];
+        };
       };
     };
 }
