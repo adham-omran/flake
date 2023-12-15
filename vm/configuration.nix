2171041778d598b3e4d5e775fa9db8f3f8cb74aa
adham-omran <git@adham-omran>
2023-12-15 10:45:12 +0300

    Add /vm

---
+++ vm/configuration.nix
@@ -0,0 +1,410 @@
+{ config, pkgs, callPackage, lib, ... }:
+let
+
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+  
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+  
+  # currently, there is some friction between sway and gtk:
+  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+  # the suggested way to set gtk settings is with gsettings
+  # for gsettings to work, we need to tell it where the schemas are
+  # using the XDG_DATA_DIR environment variable
+  # run at the end of sway config
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text = let
+      schema = pkgs.gsettings-desktop-schemas;
+      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+    in ''
+      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+      gnome_schema=org.gnome.desktop.interface
+      gsettings set $gnome_schema gtk-theme 'Dracula'
+    '';
+  };
+
+in
+{
+  imports =
+    [
+          ./hardware-configuration.nix
+          ./cachix.nix
+    ];
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+  
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Asia/Baghdad";
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.xserver = {
+    enable = true;
+    layout = "us";
+  };
+  
+   services.xserver.displayManager.gdm.enable = true;
+   services.xserver.desktopManager = {
+     gnome.enable = true;
+     plasma5.enable = false;
+   };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  
+   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+   programs.dconf.enable = true;
+   environment = {
+     plasma5.excludePackages = with pkgs.libsForQt5; [
+       elisa
+     ];
+   
+     gnome.excludePackages = (with pkgs; [
+       gnome-photos
+       gnome-tour
+     ]) ++ (with pkgs.gnome; [
+       gnome-music
+       gnome-terminal
+       gedit
+       epiphany
+       geary
+       gnome-characters
+       totem
+       tali
+       iagno
+       hitori
+       atomix
+     ]);
+   };
+  services.xserver.windowManager.dwm.enable = true;
+  programs.slock.enable = true;
+  nixpkgs.overlays = [
+    (final: prev: {
+      dwm = prev.dwm.overrideAttrs (old: { src = /home/adham/code/suckless/dwm ;});
+      slstatus = prev.slstatus.overrideAttrs (old: { src = /home/adham/code/suckless/slstatus ;});
+      dmenu = prev.dmenu.overrideAttrs (old: { src = /home/adham/code/suckless/dmenu ;});
+      st = prev.st.overrideAttrs (old: { src = /home/adham/code/suckless/st ;});
+      surf = prev.surf.overrideAttrs (old: { src = /home/adham/code/suckless/surf ;});
+      # slock = prev.surf.overrideAttrs (old: { src = /home/adham/code/suckless/slock ;});
+    })
+  ];
+  programs.hyprland.enable = true;
+  programs.browserpass.enable = true;
+  programs.light.enable = true;
+  security.polkit.enable = true;
+  
+  services.xserver.wacom.enable = true;
+  services.printing.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+  services.ipp-usb.enable = true;
+  hardware.sane.openFirewall = true;
+  services.hardware.bolt.enable = true;
+  services.tailscale.enable = true;
+  
+  services.flatpak.enable = true;
+  fonts.fontDir.enable = true;
+  
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+  
+  services.syncthing = {
+    enable = true;
+    user = "adham";
+    configDir = "/home/adham/.config/syncthing";
+  };
+  
+  services.blueman.enable = true;
+  
+  # xdg-desktop-portal works by exposing a series of D-Bus interfaces
+  # known as portals under a well-known name
+  # (org.freedesktop.portal.Desktop) and object path
+  # (/org/freedesktop/portal/desktop).
+  # The portal interfaces include APIs for file access, opening URIs,
+  # printing and others.
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  };
+  
+  # enable sway window manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+  
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  programs.fish.enable = true;
+  environment.shells = with pkgs; [ fish ];
+  users.users.adham = {
+    isNormalUser = true;
+    description = "adham";
+    extraGroups = [
+      "networkmanager" "wheel" "adbusers" "video"
+      "docker" "libvirtd" "lp" "scanner"
+    ];
+    packages = with pkgs; [
+      firefox
+    ];
+    shell = pkgs.fish;
+  };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "gnome3";
+  };
+  services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [ 25565 80 433 5000 3000 8080 4010 53 631 5353];
+  networking.firewall.allowedUDPPorts = [ 25565 80 433 5000 3000 8080 4010 53 631 5353];
+  networking.firewall.enable = true;
+  environment.systemPackages = with pkgs; [
+    
+    autotiling
+    wmenu
+    foot
+    dbus-sway-environment
+    configure-gtk
+    wayland
+    xdg-utils
+    glib # gsettings
+    dracula-theme
+    gnome3.adwaita-icon-theme
+    swaylock
+    swayidle
+    grim
+    slurp
+    wl-clipboard
+    mako
+    wdisplays
+  
+    (python3.withPackages(ps: with ps; [ pandas requests
+                                          epc orjson
+                                          sexpdata six
+                                          setuptools paramiko
+                                          rapidfuzz
+                                        ]))
+  
+    hyprpaper
+      canon-cups-ufr2
+      OVMFFull
+      slstatus
+      st
+      surf
+      tabbed
+      dmenu
+      unzip
+      cmatrix
+      libsForQt5.okular
+      rsync
+  
+      openssl
+      pinentry
+      pinentry-gtk2
+      pinentry-gnome
+      syncthing
+      killall
+      virt-manager
+      gnome.adwaita-icon-theme
+      gnomeExtensions.appindicator
+      gnome.gnome-tweaks
+    ((emacsPackagesFor emacs29-pgtk).emacsWithPackages (epkgs:
+      [
+              epkgs.vterm
+              epkgs.jinx
+      ]))
+    ];
+  services.mpd.user = "userRunningPipeWire";
+  systemd.services.mpd.environment = {
+    XDG_RUNTIME_DIR = "/run/user/1000";
+  };
+  services.kanata.enable = true;
+  services.kanata.package = pkgs.kanata;
+  
+  services.kanata.keyboards.usb.devices = [
+    "/dev/input/by-id/usb-SONiX_USB_DEVICE-event-kbd" ## external keyboard
+    "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+  ];
+  
+  services.kanata.keyboards.usb.config = ''
+  (defvar
+    tap-timeout   150
+    hold-timeout  150
+    tt $tap-timeout
+    ht $hold-timeout
+    )
+  
+  (defalias
+    qwt (layer-switch qwerty)
+    col (layer-switch colemak)
+    a (tap-hold $tt $ht a lmet)
+    r (tap-hold $tt $ht r lalt)
+    s (tap-hold $tt $ht s lctl)
+    t (tap-hold $tt $ht t lsft)
+  
+    n (tap-hold $tt $ht n rsft)
+    e (tap-hold $tt $ht e rctl)
+    i (tap-hold $tt $ht i ralt)
+    o (tap-hold $tt $ht o rmet)
+  
+    )
+  
+  (defsrc
+      esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  del
+      grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+      tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+      caps a    s    d    f    g    h    j    k    l    ;    '    ret
+      lsft z    x    c    v    b    n    m    ,    .    /    rsft
+      lctl lmet lalt           spc            ralt    rctl
+      )
+  
+  (deflayer colemak
+      esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  del
+      grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+      tab  q    w    f    p    g    j    l    u    y    ;    [    ]    \
+      caps @a   @r   @s  @t    d    h   @n   @e   @i    @o    '    ret
+      lsft z    x    c    v    b    k    m    ,    .    /    rsft
+      lctl lmet lalt           spc            @qwt    rctl
+      )
+  
+  (deflayer qwerty
+      esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  del
+      grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+      tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+      caps a    s    d    f    g    h    j    k    l    ;    '    ret
+      lsft z    x    c    v    b    n    m    ,    .    /    rsft
+      lctl lmet lalt           spc            @col    rctl
+      )
+  '';
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      amiri
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      font-awesome
+      fira-code
+      fira-code-symbols
+      scheherazade-new
+      jetbrains-mono
+      hack-font
+  
+      source-han-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+  
+      vazir-fonts
+    ];
+  
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Sans" "Noto Naskh Arabic"];
+        sansSerif = [ "Noto Sans" "Noto Naskh Arabic" ];
+        monospace = [ "JetBrains Mono" ];
+      };
+    };
+  };
+    programs.tmux = {
+      enable = true;
+  
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+  
+    extraConfig = ''
+              set -g default-terminal "xterm-256color"
+              set -ga terminal-overrides ",*256col*:Tc"
+              set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+              set-environment -g COLORTERM "truecolor"
+                '';
+  };
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+  
+    settings = {
+      START_CHARGE_THRESH_BAT0=75;
+      STOP_CHARGE_THRESH_BAT0=95;
+  
+      START_CHARGE_THRESH_BAT1=75;
+      STOP_CHARGE_THRESH_BAT1=95;
+  
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+  virtualisation = {
+    docker.enable = true;
+    waydroid.enable = true;
+    lxd.enable = true;
+    libvirtd.enable = true;
+  };
+  
+  programs.adb.enable = true;
+  system.stateVersion = "23.11";
+  nixpkgs.config.permittedInsecurePackages = [
+    "zotero-6.0.26"
+  ];
+  nixpkgs.config.allowUnfree = true;
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+  
+  nix.settings.substituters = [ "https://aseipp-nix-cache.freetls.fastly.net" ];
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+  systemd.services.NetworkManager-wait-online.enable = false;
+  systemd.user.services.mailfetch = {
+    enable = true;
+    description = "Automatically fetches for new mail when the network is up";
+    after = [ "network-online.target" ];
+    wantedBy = [ "network-online.target" ];
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "60";
+    };
+    path = with pkgs; [ bash notmuch isync ];
+    script = ''
+        mbsync -a
+      '';
+  };
+  systemd.extraConfig = ''
+  DefaultTimeoutStopSec=10sec
+  '';
+}
