eb960c51a5f74386c8dbcc097b18f7fc81a08654
adham-omran <git@adham-omran.com>
2024-02-26 18:43:57 +0300

    Add hugo

--- t480/configuration.nix
+++ t480/configuration.nix
@@ -149,6 +149,7 @@ in
   systemd.targets.multi-user.wants = [ "warp-svc.service" ];
   environment.systemPackages = with pkgs; [
     cloudflare-warp
+    hugo
     # packages from home-manager
     vnstat
     nil
