06e35772ee1d7427a6af5805c1afef6dd0000ccb
adham-omran <git@adham-omran.com>
2024-02-28 05:50:39 +0300

    Remove unused bindings

--- flake.nix
+++ flake.nix
@@ -10,13 +10,9 @@
       };
     };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
