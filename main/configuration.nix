47a3d3d625df94342be87007a3b632804dbbfe2d
adham-omran <git@adham-omran.com>
2024-03-18 17:29:00 +0300

    Remove sway

--- main/configuration.nix
+++ main/configuration.nix
@@ -1,31 +1,4 @@
 { config, pkgs, ... }:
-let
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnome.desktop.interface
-      gsettings set $gnome_schema gtk-theme 'Dracula'
-    '';
-  };
-in
 {
   imports =
     [
@@ -335,7 +308,6 @@ in
     # git
     git
     # Tiling WM Utilities
-    configure-gtk
     dbus
     foot
     glib # gsettings
@@ -343,8 +315,6 @@ in
     grim
     hyprpaper
     slurp
-    swayidle
-    swaylock
     waybar
     wayland
     wayland
