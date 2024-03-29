a8bfc4ddc455c5db3bb51c44a548a27c62903baf
adham-omran <git@adham-omran.com>
2024-03-29 18:33:30 +0300

    Remove jack

--- main/configuration.nix
+++ main/configuration.nix
@@ -32,20 +32,6 @@
   # services
   services = {
     desktopManager.plasma6.enable = true;
-    jack = {
-      jackd.enable = true;
-      # support ALSA only programs via ALSA JACK PCM plugin
-      alsa.enable = false;
-      # support ALSA only programs via loopback device (supports programs like Steam)
-      loopback = {
-        enable = true;
-        # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-        #dmixConfig = ''
-        #  period_size 2048
-        #'';
-      };
-    };
-
     ollama = {
       enable = true;
       acceleration = "rocm";
