91e42cb9b942c5d61998b7885b5a34f86167a949
adham-omran <git@adham-omran.com>
2024-04-25 22:41:21 +0300

    Update libvirtd configuration

--- main/configuration.nix
+++ main/configuration.nix
@@ -463,7 +463,21 @@
   virtualisation = {
     docker.enable = true;
     lxd.enable = true;
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [(pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd];
+        };
+      };
+    };
   };
 
   system.stateVersion = "23.11";
