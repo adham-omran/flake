ae8163f0154cc83d15a879a69c40b961eb58a6b5
adham-omran <git@adham-omran.com>
2024-05-08 19:22:58 +0300

    Update and trim down vm/home.nix

--- vm/home.nix
+++ vm/home.nix
@@ -1,136 +1,12 @@
-{pkgs, ...}:
+{pkgs, config, lib, ...}:
 {
   home = {
     stateVersion = "23.11";
     packages = with pkgs; [
       nil
-      sunshine
-      moonlight-qt
-      barrier
-      mpv
-      ffmpeg
-      qpwgraph
-      playerctl
-      ncmpcpp
-      clojure
-      babashka
-      leiningen
-      clojure-lsp
-      graalvm-ce
-      multimarkdown
-      activitywatch
-      imagemagick
-      python311Packages.pyclip
-      firejail
-      lilypond
-      ncdu
-      rclone
-      mysql80
-      awscli2
-
-      scream
-      zoom-us
-
-      isync
-      msmtp
-      afew
-      notmuch
-
-      mpc-cli
-
-      ripgrep
-      texlive.combined.scheme-full
-
-      poppler_utils
-
-      warpd
-      distrobox
-
-      hunspell
-      hunspellDicts.en_US
-
-      yt-dlp
-      gnuplot
-      libnotify
-      direnv
-      gtk3
-      graphviz
-      openssl
-      git
-      stow
-      tree
-      ledger
-      neofetch
-      bat
-      btop
-      fd
-      dmidecode
-      powertop
-      wget
-      brightnessctl
-      pavucontrol
-      pfetch
-      pass
-      element-desktop-wayland
-      zeal
-      freetube
-      gimp-with-plugins
-      qbittorrent
-      chromedriver
-      zotero
-      libreoffice-qt
-      reaper
-      nyxt
-      anki-bin
-      frescobaldi
-      sonobus
-      vlc
-      nodejs_20
-      cool-retro-term
-
-      xournalpp
-
-      google-chrome
-      geckodriver
-
-      libsForQt5.gwenview
-      krusader
-
-      obs-studio
-      poedit
-      foliate
-
-      zulip
-      telegram-desktop
-      spotify
-      networkmanagerapplet
-      cliphist
-      foot
+      wl-clipboard
       fuzzel
     ];
-  };
-  services.mpd = {
-    enable = true;
-    musicDirectory = "/home/adham/music";
-    network.port = 9900;
-    extraConfig = ''
-    audio_output {
-    type "pipewire"
-    name "My PipeWire Output"
-    }
-    '';
-
-    network.listenAddress = "any";
-    network.startWhenNeeded = true;
-    };
-  services.mpdris2 = {
-    enable = true;
-    mpd.host = "127.0.0.1";
-    mpd.port = 9900;
-  };
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode.fhs;
   };
 
   programs.git = {
@@ -143,22 +19,56 @@
     };
   };
 
-  programs.ncmpcpp = {
-    enable = true;
-    mpdMusicDir = "/home/adham/music";
-    settings = {
-      mpd_host = "127.0.0.1";
-      mpd_port = 9900;
-      execute_on_song_change = "notify-send \"Now Playing\" \"$(mpc -p 9900 --format '%title% \\n%artist% - %album%' current)\"";
-    };
-  };
-
   services.blueman-applet.enable = true;
-  programs.sioyek = {
+
+  wayland.windowManager.sway = {
     enable = true;
+    wrapperFeatures.gtk = true;
     config = {
-      "should_launch_new_window" = "1";
-      "shared_database_path" = "/home/adham/docs/sioyek-shared/shared.db";
+      startup = [
+        { command = "emacs --daemon"; }
+        { command = "foot -s"; }
+        { command = "wl-paste --watch cliphist store"; }
+      ];
+      keybindings =
+        let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in lib.mkOptionDefault {
+          "${modifier}+c" = "kill";
+          "${modifier}+Shift+n" = "move left";
+          "${modifier}+Shift+e" = "move down";
+          "${modifier}+Shift+i" = "move up";
+          "${modifier}+Shift+o" = "move right";
+          "${modifier}+n" = "focus left";
+          "${modifier}+e" = "focus down";
+          "${modifier}+i" = "focus up";
+          "${modifier}+o" = "focus right";
+
+          "${modifier}+t" = "exec emacsclient -c";
+        };
+      gaps.inner = 10;
+      fonts = {
+        names = [ "JetBrains Mono" ];
+        size = 12.0;
+      };
+      terminal = "foot";
+      modifier = "Mod4";
+      bars = [
+        {
+          position = "top";
+        }
+      ];
+      menu = "fuzzel";
+      output = {
+        "*" = {
+          background = "/mnt/pool/home/adham/pics/wall/babylon.jpg fill";
+        };
+        DP-1 = {
+          pos = "2560 0";
+          res = "2560x1440@120Hz";
+          scale = "1";
+        };
+      };
     };
   };
 }
